c27c9e12dae43597003e711ea586a8d92bc15bc6
Zaw <zaw@aluto-benli.com>
2016-03-30 16:44:09 +0630

    Small php user form

---
+++ Form.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+</head>
+<body>
+    <center>
+    <?php
+        $FNameErr= $LNameErr= $EmailErr= $GenderErr= "";
+        $FName= $LName= $Email= $Gender= "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if(empty($_POST["first name"]))
+            {
+                $FNameErr="Name is Required";
+            }
+            else{
+                $FName= test_input($_POST["first name"]);
+            }
+
+        if(empty($_POST["last name"]))
+        {
+            $LNameErr="Name is required";
+        }
+        else {
+            $LName=test_input($_POST["last name"]);
+        }
+        if(empty($_POST["email"]))
+        {
+            $EmailErr="Email is required";
+        }
+            else {
+                $Email=test_input($_POST["email"]);
+            }
+        if(empty($_POST["Gender"]))
+        {
+            $GenderErr="Exact Gender is required"; }
+        else {
+            $Gender=test_input($_POST["Gender"]);
+        }
+        }
+        ?>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <h1>A simple html form</h1>
+First Name:<br>
+<input type="text" name="first name">
+         <span class="error">* <?php echo $FNameErr;?></span><br><br>
+Last Name:<br>
+<input type="text" name="last name">
+        <span class="error">* <?php echo $LNameErr;?></span><br><br>
+Email Address:<br>
+<input type="text" name="email" >
+        <span class="error">* <?php echo $EmailErr;?></span><br><br>
+Gender:<input type="radio" value="male" name="male">male
+        <input type="radio" value="female" name="female">female
+        <span class="error">* <?php echo $GenderErr;?></span><br>
+        <input type="submit" name="submit" value="Submit">
+<input type="reset" name="clear" value="cancle"><br><br>
+
+
+
+</form>
+        </body>
+    </html>
