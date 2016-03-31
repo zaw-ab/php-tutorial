22f2e08ba7d42c27ae9b3256d6686dbda48e80b4
Zaw <zaw@aluto-benli.com>
2016-03-31 16:40:35 +0630

    Fixing some coding rule to the last php form.

---
+++ FixCodingStyle.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+</head>
+<body>
+    <?php
+    $firstNameError = "";
+    $lastNameError  = "";
+    $genderError    = "";
+
+    $firstName      = "";
+    $lastName       = "";
+    $gender         = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["first_name"])) {
+            $firstNameError = "Name is required";
+        } else {
+            $firstName = test_input($_POST["first_name"]);
+        }
+        if (empty($_POST["last_name"])) {
+            $lastNameError = "Name is required";
+        } else {
+            $lastName = test_input($_POST["last_name"]);
+        }
+        if (empty($_POST["Gender"])) {
+            $genderError = "Gender is required";
+        } else {
+            $gender = test_input($_POST["Gender"]);
+        }
+    }
+    ?>
+    <form class="form" action = "process.php"  method="post">
+        First Name:<br>
+        <input type="text" name="first_name">
+        <spam>*<?php echo $firstNameError; ?></spam><br><br>
+        Last Name:<br>
+        <input type="text" name="last_name">
+        <spam>*<?php echo $lastNameError;?></spam><br><br>
+        Gender:<br>
+        <input type="radio" name="Gender" value="male">Male
+        <input type="radio" name="Gender" value="female">Female
+        <spam>*<?php echo $genderError;?></spam><br><br>
+        <input type="submit" name="submit" value="Submit">
+        <input type="reset" name="reset" value="Clear">
+    </form>
+</body>
+    </html>
