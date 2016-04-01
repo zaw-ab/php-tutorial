e4f5bcb9f11799683326469c06ab0774b4c8cf2b
Zaw <zaw@aluto-benli.com>
2016-04-01 16:49:53 +0630

    cutting some html tags

--- dbConnect.php
+++ dbConnect.php
@@ -1,12 +1,6 @@
-<html>
-<head>
-</head>
-<body>
     <?php
-    $connect = mysqli_connect('localhost', 'abcd', 'abcdefg', 'dbs');
+    $connect = mysqli_connect('localhost', 'root', 'root', 'dbs');
     if (mysqli_connect_errno($connect)) {
         echo 'Failed to connect';
     }
     ?>
-</body>
-</html>
