4b81a79ec9ebee5753c414f9b9e7087173f4b0b8
Zaw <zaw@aluto-benli.com>
2016-04-01 16:51:18 +0630

    fixing some rues and adding some words

--- process.php
+++ process.php
@@ -1,9 +1,5 @@
-<html>
-<head>
-</head>
-<body>
-    <?php include 'dbConnect.php';?>
-        <?php
+<?php include 'dbConnect.php';?>
+    <?php
         $firstname = $_POST['first_name'];
         $lastname  = $_POST['last_name'];
         $gender    = $_POST['Gender'];
@@ -11,13 +7,12 @@
         mysqli_query($connect, "INSERT INTO user(First_Name,Last_Name,Gender)
          VALUES('$firstname','$lastname','$gender')");
 
-        if (mysqli_affected_rows($connect) > 0) {
-            echo "<p>*User have been Added to the database*</p>";
-            echo "<a href = 'FixCodingStyle.php'>Go Back</a>";
-        } else {
-                echo "User have NOT been Added<br/>";
+    if (mysqli_affected_rows($connect) > 0) {
+            echo "<p>***Congratz! User have been Added to the database***</p>";
+            echo "<a href = 'FormHandling.php'>Go Back</a>";
+    } else {
+                echo "Sorry!User have NOT been Added<br/>";
                 echo mysqli_error($connect);
-        }
+                echo "<a href = 'FormHandling.php'>Go Back</a>";
+    }
     ?>
-</body>
-</html>
