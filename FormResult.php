71bf92e585157e0cfa60a5c9244fd557ae9ea62b
Zaw <zaw@aluto-benli.com>
2016-04-04 14:32:44 +0630

    A small php file to show success or failure

---
+++ FormResult.php
@@ -0,0 +1,10 @@
+<?php
+$success = $_GET['success'];
+
+if ($success) {
+    echo "<p>***Congratz! User have been Added to the database***</p>";
+    echo "<a href = 'FormHandling.php'>Go Back</a>";
+} else {
+    echo "Sorry!User have NOT been Added<br/>";
+    echo "<a href = 'FormHandling.php'>Go Back</a>";
+}
