0c293d8a6a8e7722d2df177dbea46cf9daaf2bca
Zaw <zaw@aluto-benli.com>
2016-04-04 16:49:29 +0630

    modifying for new input data

--- process.php
+++ process.php
@@ -3,8 +3,8 @@ include 'dbConnect.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $sql = 'INSERT INTO users (first_name, last_name, gender) VALUES ("%s", "%s", "%s")';
-    $sql = sprintf($sql, $input['first_name'], $input['last_name'], $input['gender']);
+    $sql = 'INSERT INTO users (first_name, last_name, email, comment, gender) VALUES ("%s", "%s", "%s", "%s", "%s")';
+    $sql = sprintf($sql, $input['first_name'], $input['last_name'], $input['email'], $input['comment'], $input['gender']);
     mysqli_query($connect, $sql);
 
     if (mysqli_affected_rows($connect) > 0) {
