f391fd0b220938aed9a1d9d5c06eccbcaf520499
Zaw <zaw@aluto-benli.com>
2016-04-04 16:50:06 +0630

    modifying to add 2 more text input

--- FormHandling.php
+++ FormHandling.php
@@ -1,14 +1,16 @@
 <?php
 include 'functions.php';
 
-$errors = array();
-$input  = array();
+$errors  = array();
+$input   = array();
 $success = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input['first_name']    = sanitize($_POST['first_name']);
     $input['last_name']     = sanitize($_POST['last_name']);
     $input['gender']        = isset($_POST['gender']) ? sanitize($_POST['gender']) : null;
+    $input['email']         = sanitize($_POST['email']);
+    $input['comment']       = sanitize($_POST['comment']);
 
     if (empty($input['first_name'])) {
         $errors[] = "First Name is required.";
@@ -20,6 +22,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($input['gender'])) {
         $errors[] = "Gender is required.";
+    }
+
+    if (empty($input['email'])) {
+        $errors[] = "Enter your Email";
     }
 
     if (count($errors) === 0) {
@@ -65,6 +71,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div>
             <label>Last Name:</label>
             <div><input type="text" name="last_name"></div>
+        </div>
+        <div>
+            <label>Email:</label>
+            <div><input type="text" name="email"></div>
+        </div>
+        <div>
+            <label>
+                Comment:
+            </label>
+            <div>
+                <textarea name="comment" rows="5" cols="40"></textarea>
+            </div>
         </div>
         <div>
         <lable>Gender:</lable>
