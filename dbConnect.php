311d069c50fa06315304788635393ad76ce06a40
Zaw <zaw@aluto-benli.com>
2016-04-06 11:20:15 +0630

    New database connecting file

--- dbConnect.php
+++ dbConnect.php
@@ -1,6 +1,11 @@
-    <?php
-    $connect = mysqli_connect('localhost', 'root', 'root', 'dbs');
-    if (mysqli_connect_errno($connect)) {
-        echo 'Failed to connect';
-    }
-    ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "dbs";
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
