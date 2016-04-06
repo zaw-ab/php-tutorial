bc3671cb82a6793c38dc01be32d122526ff73564
Zaw <zaw@aluto-benli.com>
2016-04-06 13:30:42 +0630

    A php file to select data from database

--- select.php
+++ select.php
@@ -1,56 +1,65 @@
 <?php
+include 'dbConnect.php';
+?>
+<html>
+<head>
+    <title></title>
+    <style type="text/css">
+        table
+        {
+            width: auto;
+            border: 3px solid #49ac49;
+        }
+        th
+        {
+            background-color: darkgrey;
+            width: auto;
+            border: 3px;
+        }
+        td
+        {
+            background-color: antiquewhite;
+            width:200px;
+            height: 50px;
+            text-align:center;
+        }
+    </style>
+</head>
+<body>
+<?php
+$sql = "SELECT * FROM users order by User_ID ASC";
+$result = mysqli_query($connect, $sql);
 
-    echo "<table style='width:100%;border: solid 1px blue;'>";
-    echo "<tr style='color: green;'>
-                <th>UserId</th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Gmail</th>
-                <th>Comment</th>
-                <th>Gender</th>
-                <th>edit data</th>
-            </tr>";
-
-class TableRows extends RecursiveIteratorIterator
-{
-    public function __construct($cst)
-    {
-        parent::__construct($cst, self::LEAVES_ONLY);
-    }
-
-    public function current()
-    {
-        return "<td style='width:150px;border: solid 1px blue;text-align:center;'>" . parent::current(). "</td>";
-    }
-
-    public function beginChildren()
-    {
-        echo "<tr>";
-    }
-
-    public function endChildren()
-    {
-        echo "<td style='width:100px;border: solid 1px blue;text-align:center;'><a href =''>Edit</a></td>"."</tr>";
-    }
+if (mysqli_num_rows($result) > 0) {
+?>
+    <table>
+        <tr>
+            <th>User_ID</th>
+            <th>First_Name</th>
+            <th>Last_Name</th>
+            <th>Email</th>
+            <th>Comment</th>
+            <th>Gender</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td><?php echo $row['User_ID']; ?></td>
+            <td><?php echo $row['first_name']; ?></td>
+            <td><?php echo $row['last_name']; ?></td>
+            <td><?php echo $row['Email']; ?></td>
+            <td><?php echo $row['comment']; ?></td>
+            <td><?php echo $row['gender']; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+<?php
+} else {
+?>
+    <div>There is no user.</div>
+<?php
 }
-
-    $servername = "localhost";
-    $username   = "root";
-    $password   = "root";
-    $dbname     = "dbs";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM users order by User_ID ASC");
-    $stmt->execute();
-
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
-            echo $v;
-    }
-} catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-}
-    $conn = null;
-    echo "</table>";
+?>
+</body>
+</html>
+<?php
+mysqli_close($connect);
