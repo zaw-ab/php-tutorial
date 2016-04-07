307f44aecfcb7bd4d41603747ec329932384127a
Zaw <zaw@aluto-benli.com>
2016-04-07 16:52:16 +0630

    nothing changed

--- FormResult.php
+++ FormResult.php
@@ -6,7 +6,6 @@ if ($success) {
     echo "<p>***Congratz! User have been Added to the database***</p>";
     echo "<a href = 'FormHandling.php'>Go Back</a>";
 } else {
-    echo "Sorry!User have NOT been Added<br/>
-          Your Email have been used<br/>";
+    echo "Sorry!User have NOT been Added<br/>";
     echo "<a href = 'FormHandling.php'>Go Back</a>";
 }
