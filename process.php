00d7b488c0d9054f763b5430176f1acc2615c10c
Zaw <zaw@aluto-benli.com>
2016-04-07 16:51:18 +0630

    adding codes for update data

--- process.php
+++ process.php
@@ -1,10 +1,14 @@
 <?php
-include 'dbConnect.php';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $sql = 'INSERT INTO users (first_name, last_name, email, comment, gender) VALUES ("%s", "%s", "%s", "%s", "%s")';
-    $sql = sprintf($sql, $input['first_name'], $input['last_name'], $input['email'], $input['comment'], $input['gender']);
+    if ($_POST['id']) {
+        // update
+        $sql= "UPDATE users ". "SET first_name= $input['first_name'] ". "SET last_name= $input['last_name'] ". "SET Email= $input['email'] ". "SET comment= $input['comment'] ". "SET gender= $input['gender'] ". "WHERE User_ID= $userId ";
+    } else {
+        // insert
+        $sql = 'INSERT INTO users (first_name, last_name, email, comment, gender) VALUES ("%s", "%s", "%s", "%s", "%s")';
+        $sql = sprintf($sql, $input['first_name'], $input['last_name'], $input['email'], $input['comment'], $input['gender']);
+    }
     mysqli_query($connect, $sql);
 
     if (mysqli_affected_rows($connect) > 0) {
