b3375aea4d1bcc07527aa9011b6635495f94e9d0
Zaw <zaw@aluto-benli.com>
2016-04-08 17:00:35 +0630

    adding updating process

--- process.php
+++ process.php
@@ -1,19 +1,50 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = $input['first_name'];
+    $lastname  = $input['last_name'];
+    $email     = $input['email'];
+    $comment   = $input['comment'];
+    $gender    = $input['gender'];
+    $userId    = $_POST['id'];
 
-    if ($_POST['id']) {
+    if (isset($_POST['id'])) {
+        //test $userID
+     if($_POST['id']) {
+            //echo "$userId";exit;
+        }
+
         // update
-        $sql= "UPDATE users ". "SET first_name= $input['first_name'] ". "SET last_name= $input['last_name'] ". "SET Email= $input['email'] ". "SET comment= $input['comment'] ". "SET gender= $input['gender'] ". "WHERE User_ID= $userId ";
+        $sql = '
+            UPDATE users SET
+                first_name = "' . $firstname . '",
+                last_name = "' . $lastname . '",
+                Email      = " ' . $email . ' ",
+                comment    = " ' . $comment . ' ",
+                gender     = "' . $gender . ' "
+            WHERE User_ID = ' . $userId;
+        ///$sql= "UPDATE users ". "SET first_name= $firstname ". "SET last_name= $lastname ". "SET Email= $email ". "SET comment= $comment ". "SET gender= $gender ". "WHERE User_ID= $userId ";
+        //echo $sql; exit;
+        $update =mysqli_query($connect, $sql);
+
+        if (!$update) {
+            $success= false;
+            echo mysqli_error($connect);
+            echo "Invalid user data";
+            exit;
+        } else {
+            $success= true;
+            echo "User Data have been updated";
+        }
     } else {
         // insert
-        $sql = 'INSERT INTO users (first_name, last_name, email, comment, gender) VALUES ("%s", "%s", "%s", "%s", "%s")';
-        $sql = sprintf($sql, $input['first_name'], $input['last_name'], $input['email'], $input['comment'], $input['gender']);
-    }
-    mysqli_query($connect, $sql);
+        $ssql = 'INSERT INTO users (first_name, last_name, Email, comment, gender) VALUES ("%s", "%s", "%s", "%s", "%s")';
+        $ssql = sprintf($ssql, $firstname, $lastname, $email, $comment, $gender);
+        $insert= mysqli_query($connect, $ssql);
 
-    if (mysqli_affected_rows($connect) > 0) {
-        $success = true;
-    } else {
-        $success = false;
+        if (!$insert) {
+            $success = false;
+        } else {
+            $success = true;
+        }
     }
 }
